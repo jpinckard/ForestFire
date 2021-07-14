7a141f96e1899bd2d90f884b6b3b6c325be00797
Joy Pinckard <joypinckard@gmail.com>
2021-07-14 17:07:09 -0500

    Add files via upload
    Added burning simulation.

--- forestfire.py
+++ forestfire.py
@@ -1,5 +1,5 @@
 import plotly.graph_objects as go
-import random
+import random 
 import animation
 import math
 import numpy as np
@@ -26,6 +26,8 @@ b = 0.05
 min_sep = 4
 time=120 # simulation length
 
+dist = 3 # distance between cells to check
+
 # Terrain extents defined by south-west and north-east points
 # Currently for huntsville area, please change these to a selected forest later
 MIN_LON = -86.90567
@@ -33,13 +35,19 @@ MIN_LAT = 34.53765
 MAX_LON = -86.41380
 MAX_LAT = 34.86531
 
+
+# For converting cell positions to latitude and longitude.
+area_width  = MAX_LAT - MIN_LAT
+area_height = MAX_LON - MIN_LON
+
+x_scale = grid_width  / area_width
+y_scale = grid_height / area_height
+
 # Constants used to calculate the probability of ignition due to elevation and wind.
 C1 = float(0.045)
 C2 = float(0.131)
 A  = float(0.078)
 
-
-frames   = [] # Frames of animation. A collection of grid info.
 trees    = [] # x, y coordinate of each tree
 p_ignite = []
 
@@ -47,7 +55,6 @@ colors = ["Black", "Red", "Green"]
 unburned  = 2
 burning   = 1
 burnedout = 0
-
 
 TREES_X = 0 # Position of x variable in trees array
 TREES_Y = 1 # position of y variable in trees array
@@ -87,8 +94,7 @@ def probability_wind(speed, wind_angle, fire_angle):
 def probability_elevation(e1, e2, dist):
     return np.exp(A * np.arctan((e1 - e2)/dist))
 
-def Distance(tree1, tree2):
-
+def Distance(tree1_x, tree1_y, tree2_x, tree2_y):
     dist = math.sqrt((tree2[TREES_X]-tree1[TREES_X])**2 + (tree2[TREES_Y]-tree2[TREES_Y])**2)
     return dist
 
@@ -100,102 +106,21 @@ def InitGridForest(grid_width, grid_height):
     
     trees = np.empty((grid_width, grid_height, 2))
 
-    area_width  = MAX_LAT - MIN_LAT
-    area_height = MAX_LON - MIN_LON
-
-    x_scale = grid_width  / area_width
-    y_scale = grid_height / area_height
-
-    t = TerrainApi()
+    #t = TerrainApi()
 
     for y, row in enumerate (trees):
         for x, col in enumerate (row):
             pos = [(x * x_scale) + MIN_LAT, (y * y_scale) + MIN_LON]
-            trees[y][x] = [t.get_terrain_altitude(0, pos[0], pos[1]), unburned]    
+            #z = t.get_terrain_altitude(pos[0], pos[1])
+            trees[y][x] = [0, unburned]    
 
 
-    trees[grid_width/2][grid_height/2] = [TerrainAPI.get_terrain_altitude(grid_width/2, grid_height/2), burning]    
+    #z = TerrainAPI.get_terrain_altitude(grid_width/2, grid_height/2)
+    x = int(np.floor(grid_width/2))
+    y = int(np.floor(grid_height/2))
+    trees[y][x] = [0, burning] 
 
     return trees
-
-
-########################
-## INITIALIZE FOREST ###
-# Creates initial forest randomly located in 2D rectangle.
-# No two trees are less than MinDistance apart.
-def InitForest(grid_height, grid_width, num_trees, min_sep):
-
-    # Generate a forest of trees at random positions.
-    trees = np.array([[grid_width/2, grid_height/2, burning]]) # all trees are initially unburned
-
-    i=0
-    while (len(trees) < num_trees):
-
-        tree_x = np.random.uniform(0,grid_width,1)[0]
-        tree_y = np.random.uniform(0,grid_height,1)[0]
-        state  = unburned
-
-        tree = [tree_x, tree_y, state]
-
-        tooclose = False;
-
-        i = 0
-        n = len(trees)
-        while i < n-1:
-          distance = Distance(tree, trees[i])
-          #print("Checking!")
-          if (distance < min_sep):
-              tooclose = True;
-              break;
-          i += 1
-
-        if (tooclose == False):
-            #print(tree)
-            trees = np.vstack([trees, tree])
-         
-    # Create initial burning tree
-    trees = np.vstack([trees, [grid_width/2, grid_height/2, burning]])
-    
-    return trees
-
-#############################
-## O(n) INITIALIZE FOREST ###
-## n = 500
-## sensitivity = 0.8 # 0 means no movement, 1 means max distance is init_dist
-def InitPointForest(n, sensitivity):
-
-    shape = np.array([grid_width, grid_height])
-    
-    # compute grid shape based on number of points
-    width_ratio = shape[1] / shape[0]
-    num_y = np.int32(np.sqrt(n / width_ratio)) + 1
-    num_x = np.int32(n / num_y) + 1
-
-    # create regularly spaced neurons
-    x = np.linspace(0., shape[1]-1, num_x, dtype=np.float32)
-    y = np.linspace(0., shape[0]-1, num_y, dtype=np.float32)
-    coords = np.stack(np.meshgrid(x, y), -1).reshape(-1,2)
-
-    # compute spacing
-    init_dist = np.min((x[1]-x[0], y[1]-y[0]))
-    min_dist = init_dist * (1 - sensitivity)
-
-    assert init_dist >= min_dist
-    print(min_dist)
-
-    # perturb points
-    max_movement = (init_dist - min_dist)/2
-    noise = np.random.uniform(
-        low=-max_movement,
-        high=max_movement,
-        size=(len(coords), 2))
-    coords += noise
-
-    plt.figure(figsize=(10*width_ratio,10))
-    plt.scatter(coords[:,0], coords[:,1], s=3)
-    plt.show()
-
-    return shape
 
 ############################
 ## PROBABILITY TO IGNITE ###
@@ -207,8 +132,8 @@ def InitPignite(trees, num_trees, a, r_inner, r_outer):
 
     p_ignite = np.empty([N, N]) # Creates an NxN array to hold all the tree probs
     
-    for i in range (0, N):
-        for j in range (0, N):
+    for i in range (N):
+        for j in range (N):
             dist_ij = Distance(trees[i], trees[j])
             if (dist_ij >= r_outer):
                 p_ignite[i, j] = 0
@@ -218,6 +143,7 @@ def InitPignite(trees, num_trees, a, r_inner, r_outer):
                 p_ignite[i, j] = a * (1 - ((dist_ij - r_inner)/(r_outer - r_inner) ))
 
     return p_ignite
+
 
 ############################
 ## BURN FOREST #############
@@ -230,21 +156,23 @@ def BurnForest(trees, num_trees, grid_width, grid_height, time, a, b, p_ignite, 
     for i in range (time):
         nextTrees = trees
         j=0
-        for j in range (num_trees-1):
-            if (trees[j][TREES_STATE] == burning ):
-                if (np.random.uniform(0,1,1)[0] < b):
-                    nextTrees[j][TREES_STATE] = burnedout
-                    burnedoutCount += 1
-                    burningCount -= 1
-            elif (trees[j][TREES_STATE] == unburned ):
-                Q=1
-                k=0
-                for k in range (num_trees-1):
-                    if (trees[k][TREES_STATE] == burning ):
-                        Q = Q * (1-p_ignite[j,k])
-                if (np.random.uniform(0,1,1)[0] >= Q):
-                    nextTrees[j][TREES_STATE] = burning
-                    burningCount += 1
+        for y, row in enumerate (trees):
+            for x, col in enumerate (row):
+                if (trees[y][x][1] == burning ):
+                    if (np.random.uniform(0,1,1)[0] < b):
+                        nextTrees[y][x][1] = burnedout
+                        burnedoutCount += 1
+                        burningCount -= 1
+                elif (trees[y][x][1] == unburned ):
+                    Q=1
+                    k=0
+                    for u, row in enumerate (trees):
+                       for w, col in enumerate (row):
+                        if (trees[u][w][1] == burning ):
+                            Q = Q * (1-p_ignite[u, w])
+                    if (np.random.uniform(0,1,1)[0] >= Q):
+                        trees[y][x][1] = burning
+                        burningCount += 1
 
         trees = nextTrees
         if (i % 3 == 0):
@@ -253,20 +181,102 @@ def BurnForest(trees, num_trees, grid_width, grid_height, time, a, b, p_ignite, 
     #results =[num_trees - burningCount - burnedoutCount, burningCount, burnedoutCount]
     return nextTrees
 
+############################
+## JOY BURN FOREST #############
+# Simulates forest fire using SIR model with spatial ignition probability.
+def BurnForest(trees, num_trees, grid_width, grid_height, dist, time):
+
+    x = int(np.floor(grid_width/2))
+    y = int(np.floor(grid_height/2))
+    
+    burning_trees = [[x, y]] # list of indexes of burning trees from list 'trees'. Initially populated with the first burning tree.
+    burnt_trees = [[0,0]] # list of indexes of burning trees from list 'trees'. Initially populated with the first burning tree.
+    turns = [[0, burnt_trees, burning_trees]] # A record of the changes in tree state for each turn.
+
+    
+
+    # Burn over time
+    for turn in range (time):
+
+        print ("Turn: ", turn)
+        print ("Burning trees", burning_trees)
+        print ("Length burning trees: ", len(burning_trees))
+
+        burning_trees_len = len(burning_trees)
+
+        if (len(burning_trees) <= 0):
+            break
+
+
+
+        #print ("Burning trees", burning_trees)
+        
+        # Loop through all burning trees
+        for j in range(burning_trees_len):
+
+            print ("j", j)
+
+            tree = burning_trees[j]
+
+            #print (tree)
+
+            # Get burnt out trees
+            if (np.random.uniform(0,1,1)[0] < b):                
+                trees[tree[1], tree[0]][1] = burnedout
+                burning_trees.remove(tree) # might have to change the syntax of this
+                burnt_trees.append(tree)
+
+            # Check neighbors for ignition 
+            for i in range (-dist, dist):
+
+                print ("Burning tree num", j)
+
+                # Get neighbors along X and Y axis
+                if (i != 0):
+                    neighbor_x = [x+i, y]
+                    neighbor_y = [x, y+i]
+                    neighbor_d = [x+i, y+i] # along the diagonal
+
+                # print ("Tree", trees[neighbor_x[1]][neighbor_x[0]][1] == unburned)
+
+                # Check ignition of those neighbors
+                if (TryIgnite(trees[neighbor_x[1]][neighbor_x[0]])):
+                    burning_trees.append(neighbor_x)
+                    trees[neighbor_x[1]][neighbor_x[0]][1] = burning
+                    print("...")
+                    
+                if (TryIgnite(trees[neighbor_y[1]][neighbor_y[0]])):
+                    burning_trees.append(neighbor_y)
+                    trees[neighbor_y[1]][neighbor_y[0]][1] = burning
+                    
+                if (TryIgnite(trees[neighbor_d[1]][neighbor_d[0]])):
+                    burning_trees.append(neighbor_d)
+                    trees[neighbor_d[1]][neighbor_d[0]][1] = burning
+
+        turns.append([turn, burnt_trees, burning_trees])                
+                
+
+############################
+## TRY IGNITE ##############
+# Checks ignition probability of two trees.
+def TryIgnite(tree):
+    if (tree[1] == unburned and random.randint(0, 1) == 1):
+        print("True This ran")
+        return True
+    print ("False")
+    return False
 
 ################################################
 ## FUNCTIONS ###################################
 ##########
 ## MAIN ##
 def Main():
-    #trees = InitPointForest(num_trees, 0.8)
-    #trees = InitForest(grid_height, grid_width, num_trees, min_sep)
     trees = InitGridForest(grid_width, grid_height)
-    p_ignite = InitPignite(trees, num_trees, a, r_inner, r_outer)
-    burn_forest = BurnForest(trees, num_trees, grid_width, grid_height, time, a, b, p_ignite, 1)
-    print(burn_forest)
-    output = pd.DataFrame(burn_forest).to_csv("C:/Users/jpinckard/Documents/CS595_Summer2021_MikelPetty/ForestFire/results.csv")
-    trees = InitForest(num_trees, 0.8)
+    
+    #p_ignite = InitPignite(trees, num_trees, a, r_inner, r_outer)
+    burn_forest = BurnForest(trees, num_trees, grid_width, grid_height, dist, time)
+    #print(burn_forest)
+    #output = pd.DataFrame(burn_forest).to_csv("C:/Users/jpinckard/Documents/CS595_Summer2021_MikelPetty/ForestFire/results.csv")
 
 
 ##################
